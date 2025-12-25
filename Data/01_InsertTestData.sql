137f3fab398f29c65bc1f99fe517064aacd42393
Adchariya Changtam <adchariya.c@gmail.com>
2025-12-25 11:30:18 +0100

    Database creation, Tables creation, Test Data Insertion, Entitiy Relationship diagram first version

---
+++ Data/01_InsertTestData.sql
@@ -0,0 +1,32 @@
+INSERT INTO Members (FirstName, LastName, Email, Phone, RegistrationDate)
+VALUES
+('Alice', 'Wonderland', 'alice.wonderland@email.com', '070-1234567', '2025-01-01'),
+('Bob', 'Marley', 'bob.marley@email.com', NULL, '2025-02-02'),
+('Charlie', 'Brown', 'charlie.brown@email.com', '073-7654321', '2025-03-03');
+GO
+
+INSERT INTO Books (Title, Author, ISBN, PublicationDate, Quantity)
+VALUES
+('The Hobbit', 'J.R.R. Tolkien', '9780547928227', '1937-11-21', 2),
+('To Kill a Mockingbird ', 'Harper Lee', '9780061120084', '1960-07-11', 3),
+('Nineteen Eighty-Four', 'George Orwell', '9780201633610', '1949-06-08', 4);
+GO
+
+INSERT INTO Loans(LoanDate, DueDate, FKMemberID, FKBookID)
+VALUES
+('2025-01-01', '2025-01-15', 1, 2),
+('2025-02-02', '2025-02-16', 2, 3),
+('2026-01-01', '2026-01-15', 3, 1);
+GO
+
+INSERT INTO LoanReturns(ReturnDate, IsLate, FKLoanID)
+VALUES
+('2025-01-04', 0, 1),
+('2025-02-20', 1, 2),
+('2026-01-15', 0, 3);
+GO
+
+SELECT * FROM Members;
+SELECT * FROM Books;
+SELECT * FROM Loans;
+SELECT * FROM LoanReturns;
---
+++ Database/01_CreateDatabase.sql
@@ -0,0 +1,8 @@
+IF DB_ID('LibraryDB') IS NULL
+BEGIN
+    CREATE DATABASE LibraryDB;
+END;
+GO
+
+USE LibraryDB;
+GO
